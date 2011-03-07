a362d29d056605768a412bc54423f9263d240aaa
Matt Russell <MattRussellUK@gmail.com>
2011-03-07 07:39:02 +0000

    wip

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/semantichighlighting/SymbolInfo.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/semantichighlighting/SymbolInfo.scala
@@ -1,18 +1,18 @@
-package scala.tools.eclipse.semantichighlighting
-
-import org.eclipse.jface.text.Region
-
-case class SymbolInfo(symbolType: SymbolType, regions: List[Region])
-
-sealed trait SymbolType
-
-object SymbolTypes {
-  case object LocalVar extends SymbolType
-  case object LocalVal extends SymbolType
-  case object TemplateVar extends SymbolType
-  case object TemplateVal extends SymbolType
-  case object Method extends SymbolType
-  case object MethodParam extends SymbolType
-  case object Other extends SymbolType
-
-}
+//package scala.tools.eclipse.semantichighlighting
+//
+//import org.eclipse.jface.text.Region
+//
+//case class SymbolInfo(symbolType: SymbolType, regions: List[Region])
+//
+//sealed trait SymbolType
+//
+//object SymbolTypes {
+//  case object LocalVar extends SymbolType
+//  case object LocalVal extends SymbolType
+//  case object TemplateVar extends SymbolType
+//  case object TemplateVal extends SymbolType
+//  case object Method extends SymbolType
+//  case object MethodParam extends SymbolType
+//  case object Other extends SymbolType
+//
+//}
